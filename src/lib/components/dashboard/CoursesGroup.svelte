e57b6d05c6cb29be47332f3ff82b57c90c7f649f
imlargo <jlargob@unal.edu.co>
2025-01-27 23:54:45 -0500

    refactor: Abstraer grupo de cursos de usuario en home

---
+++ src/lib/components/dashboard/CoursesGroup.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import * as Collapsible from '$lib/components/ui/collapsible/index.js';
+	import CourseCard from '$components/dashboard/CourseCard.svelte';
+	import { Button } from '$lib/components/ui/button/index.js';
+
+	type UserCourse = {
+		name: string;
+		image: string;
+		institution: string;
+		institutionImage?: string;
+		progress: number;
+		finished: boolean;
+	};
+
+	type Props = {
+		title: string;
+		courses: UserCourse[];
+		
+	};
+
+	const { title, courses }: Props = $props();
+</script>
+
+<Collapsible.Root>
+	<Collapsible.Trigger
+		class="flex w-full items-center justify-between rounded-md px-4 py-1 hover:bg-indigo-100"
+	>
+		<h3 class="text-xl font-bold">{title}</h3>
+		<Button>
+			<i class="bi bi-chevron-down"></i>
+		</Button>
+	</Collapsible.Trigger>
+	<Collapsible.Content class="mt-8 mb-12">
+		<div class="grid grid-cols-3 gap-8">
+			{#each courses as course}
+				<CourseCard {...course} />
+			{/each}
+		</div>
+	</Collapsible.Content>
+</Collapsible.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,16 +5,18 @@
 	import CourseCard from '$components/dashboard/CourseCard.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 
+	import CoursesGroup from '$lib/components/dashboard/CoursesGroup.svelte';
+
 	type UserCourse = {
 		name: string;
 		image: string;
 		institution: string;
-        institutionImage?: string;
+		institutionImage?: string;
 		progress: number;
 		finished: boolean;
 	};
 
-	const userCourses: UserCourse[] = [
+	const userCourses1: UserCourse[] = [
 		{
 			name: 'Fundamentos de programacion',
 			image: '',
@@ -56,7 +58,77 @@
 			image: '',
 			institution: 'Universidad Nacional de Colombia',
 			progress: 70,
+			finished: false
+		}
+	];
+
+	const userCourses2: UserCourse[] = [
+		{
+			name: 'Fundamentos de programacion',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 80,
 			finished: true
+		},
+		{
+			name: 'Algoritmos y Estructuras de Datos',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 60,
+			finished: true
+		},
+		{
+			name: 'Bases de Datos',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 45,
+			finished: true
+		}
+	];
+
+	const userCourses3: UserCourse[] = [
+		{
+			name: 'Fundamentos de programacion',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 80,
+			finished: false
+		},
+		{
+			name: 'Algoritmos y Estructuras de Datos',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 60,
+			finished: false
+		},
+		{
+			name: 'Bases de Datos',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 45,
+			finished: false
+		},
+		{
+			name: 'Ingeniería de Software',
+			image: '',
+			institutionImage: 'https://github.com/shadcn.png',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 30,
+			finished: false
+		},
+		{
+			name: 'Redes de Computadores',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 90,
+			finished: false
+		},
+		{
+			name: 'Sistemas Operativos',
+			image: '',
+			institution: 'Universidad Nacional de Colombia',
+			progress: 70,
+			finished: false
 		}
 	];
 </script>
@@ -65,24 +137,10 @@
 	<h2>Bienvenida, Maria</h2>
 
 	<div class="grid grid-cols-12 gap-x-12">
-		<div class="col-span-8">
-			<Collapsible.Root>
-				<Collapsible.Trigger
-					class="flex w-full items-center justify-between rounded-md px-4 py-1 hover:bg-indigo-100"
-				>
-					<h3 class="text-xl font-bold">En progreso</h3>
-					<Button>
-						<i class="bi bi-chevron-down"></i>
-					</Button>
-				</Collapsible.Trigger>
-				<Collapsible.Content class="mt-8">
-					<div class="grid grid-cols-3 gap-8">
-						{#each userCourses as course}
-							<CourseCard {...course} />
-						{/each}
-					</div>
-				</Collapsible.Content>
-			</Collapsible.Root>
+		<div class="col-span-8 flex flex-col gap-2">
+			<CoursesGroup title="En progreso" courses={userCourses1} />
+			<CoursesGroup title="Completados" courses={userCourses2} />
+			<CoursesGroup title="Disponibles" courses={userCourses3} />
 		</div>
 
 		<aside class="col-span-4 flex flex-col gap-8">
