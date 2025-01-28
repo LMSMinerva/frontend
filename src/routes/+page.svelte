a85c883b38f521e5859fef7e09608fd12ea3557b
imlargo <jlargob@unal.edu.co>
2025-01-28 08:09:56 -0500

    feat: Agregar sección de actividades pendientes con componente checkbox

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,22 @@
 
 	const { inProgress, completed, availiable } = getUserCourses();
 	const { user } = data;
+	import { Checkbox } from '$lib/components/ui/checkbox/index.js';
+
+	const pendindActivities = [
+		{
+			curso: 'Fundamentos de programación',
+			actividad: 'Quiz 2: Ciclos for y while'
+		},
+		{
+			curso: 'Fundamentos de programación',
+			actividad: 'Quiz 2: Ciclos for y while'
+		},
+		{
+			curso: 'Fundamentos de programación',
+			actividad: 'Quiz 2: Ciclos for y while'
+		}
+	];
 </script>
 
 <main class="flex flex-col gap-8 px-12">
@@ -21,7 +37,19 @@
 		</div>
 
 		<aside class="col-span-4 flex flex-col gap-8">
-			<div class="rounded-lg bg-white px-12 py-24"></div>
+			<div class="flex flex-col gap-4 rounded-lg bg-white px-8 py-8">
+				<h3 class="text-xl font-semibold">Actividades Pendientes</h3>
+
+				{#each pendindActivities as pendingActivity}
+					<div class="flex items-top gap-2 rounded-md border-2 border-gray-200 p-4 py-4">
+						<Checkbox class="rounded-full" />
+						<div>
+							<p class="text-sm font-semibold">{pendingActivity.curso}</p>
+							<p class="text-xs text-gray-500">{pendingActivity.actividad}</p>
+						</div>
+					</div>
+				{/each}
+			</div>
 			<div class="rounded-lg bg-white px-12 py-24"></div>
 			<div class="rounded-lg bg-white px-12 py-24"></div>
 			<div class="rounded-lg bg-white px-12 py-24"></div>
