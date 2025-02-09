b6dc731cc230aec79855a450ef0b143c217fae92
imlargo <jlargob@unal.edu.co>
2025-02-09 18:50:43 -0500

    fix: remove hardcoded authToken setting in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,15 +4,6 @@ export const handle: Handle = async ({ event, resolve }) => {
 	const authToken = event.cookies.get('authToken');
 	const refreshToken = event.cookies.get('refreshToken');
 
-
-	event.cookies.set('authToken', "bWluZXJ2YTptaW5fbG1zMTIz", {
-		maxAge: 60 * 60 * 24 * 7,
-		secure: true,
-		sameSite: 'lax',
-		httpOnly: true,
-		path: '/',
-	});
-
 	const isValid = authToken !== undefined && refreshToken !== undefined;
 	const isLogin = event.url.pathname === '/login';
 
