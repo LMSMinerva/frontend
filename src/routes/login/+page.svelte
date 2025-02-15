4b263d6a8bd5d2677e5a2afab3aec5cbd21584f9
imlargo <jlargob@unal.edu.co>
2025-02-15 11:01:01 -0500

    refactor: use const instead of var for better variable scoping

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -43,15 +43,15 @@
 	 */
 	function oauthSignIn() {
 		// Google's OAuth 2.0 endpoint for requesting an access token
-		var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+		const oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
 
 		// Create <form> element to submit parameters to OAuth 2.0 endpoint.
-		var form = document.createElement('form');
+		const form = document.createElement('form');
 		form.setAttribute('method', 'GET'); // Send as a GET request.
 		form.setAttribute('action', oauth2Endpoint);
 
 		// Parameters to pass to OAuth 2.0 endpoint.
-		var params = {
+		const params: Record<string, string> = {
 			client_id: '927512412726-qbrkf1gcel5f2gnsk6tsegq8n1gjfni9.apps.googleusercontent.com',
 			redirect_uri: 'http://localhost:5173/authorize',
 			response_type: 'token',
@@ -61,8 +61,8 @@
 		};
 
 		// Add form parameters as hidden input values.
-		for (var p in params) {
-			var input = document.createElement('input');
+		for (const p in params) {
+			const input = document.createElement('input');
 			input.setAttribute('type', 'hidden');
 			input.setAttribute('name', p);
 			input.setAttribute('value', params[p]);
@@ -130,7 +130,7 @@
 			</form>
 
 			<div class="mt-4">
-				<Button variant="outline" class="w-full" disabled={isLoading} onclick={oauthSignIn} >
+				<Button variant="outline" class="w-full" disabled={isLoading} onclick={oauthSignIn}>
 					{#if isLoading}
 						<i class="bi bi-arrow-repeat animate-spin leading-none"></i>
 					{:else}
