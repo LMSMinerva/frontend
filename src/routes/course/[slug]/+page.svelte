952e3293cf59edc580ee4954d633f999fa1cbd0e
imlargo <jlargob@unal.edu.co>
2025-02-16 16:37:52 -0500

    refactor: streamline institution data fetching in course page component

--- src/routes/course/[slug]/+page.svelte
+++ src/routes/course/[slug]/+page.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import { CourseStore } from '$lib/stores/course';
-	import { InstitutionStore } from '$lib/stores/institution';
 	import type { Course } from '$lib/types/course';
+	import { InstitutionStore } from '$lib/stores/institution';
+	const institutionStore = new InstitutionStore();
+
+	
 
 	type Props = {
 		data: PageData;
@@ -12,14 +15,14 @@
 
 	const course = $state(data.course);
 	let institution: Institution | null = $state(null);
-	
-	const institutionStore = new InstitutionStore();
-	institutionStore.getInstitution(course?.institution as string).then((res) => {
-		institution = res;
+
+	onMount(async () => {
+		institution = await institutionStore.getInstitution((data.course as Course).institution);
 	});
 
 	import Home from '$lib/components/course/Home.svelte';
 	import type { Institution } from '$lib/types/institution';
+	import { onMount } from 'svelte';
 </script>
-	
-<Home course={course as Course} institution={institution} />
+
+<Home course={course as Course} {institution} />
