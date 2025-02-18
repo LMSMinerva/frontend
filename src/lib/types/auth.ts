c1176a103c7199edb53ff8fb6c9b40cc9d490553
imlargo <jlargob@unal.edu.co>
2025-02-18 07:11:13 -0500

    feat: add user profile and Google sign-in response types

--- src/lib/types/auth.ts
+++ src/lib/types/auth.ts
@@ -7,3 +7,25 @@ export type GoogleCredentialsResponse = {
 	credential: string;
 	g_csrf_token: string;
 }
+
+export type UserProfile = {
+	email: string;
+	gender: string;
+	birthday_date: string | null;
+	role: string;
+	picture: string;
+	given_name: string;
+	family_name: string;
+	locale: string;
+};
+
+export type User = {
+	id: number;
+	username: string;
+	profile: UserProfile;
+};
+
+export type GoogleSignInResponse = {
+	access_token: string;
+	user: User;
+};
