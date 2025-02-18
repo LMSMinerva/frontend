335992901a4b764ef6ee20a2400961f8a8e6c02d
imlargo <jlargob@unal.edu.co>
2025-02-18 07:25:06 -0500

    feat: update Google OAuth handling to use new response types and set authentication cookies

--- src/routes/authorize/+server.ts
+++ src/routes/authorize/+server.ts
@@ -1,11 +1,31 @@
 import { json, redirect } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
-import type { GoogleCredentialsResponse } from '$lib/types/auth';
+import type { GoogleOAuthResponse, GoogleSignInResponse } from '$lib/types/auth';
+import { apiBaseUrl } from '$lib/utils/constants';
+import { AuthCookies } from '$lib/server/auth';
 
 export const GET: RequestHandler = async ({ request, url, cookies }) => {
     const credentialsText = url.searchParams.toString();
     const decoded = new URLSearchParams(credentialsText);
-    const credentials = Object.fromEntries(decoded) as GoogleCredentialsResponse;
+    const credentials = Object.fromEntries(decoded) as GoogleOAuthResponse;
+
+    const oathCode = credentials.code;
+    const response = await fetch(`${apiBaseUrl}/login-with-google/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ code: oathCode }),
+    });
+
+    if (!response.ok) {
+        throw redirect(303, '/login');
+    }
+
+    const data: GoogleSignInResponse = await response.json();
+    const { access_token, user } = data;
+
+    AuthCookies.setAuthCookies(cookies, access_token, '');
 
     return json({ credentials });
 };
