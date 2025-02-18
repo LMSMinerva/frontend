b57225dc2811da7c912f21066dd147a0dea4cbac
imlargo <jlargob@unal.edu.co>
2025-02-18 10:14:30 -0500

    feat: add logging for authentication cookie checks and token retrieval in server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,7 +3,11 @@ import { AuthCookies } from '$lib/server/auth';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const hasAuthCookies = AuthCookies.hasAuthCookies(event.cookies);
+	const authTokens = AuthCookies.getAuthTokens(event.cookies);
 	const isLogin = event.url.pathname === '/login' || event.url.pathname === '/authorize';
+
+	console.log('hasAuthCookies', hasAuthCookies);
+	console.log('authTokens', authTokens.accessToken);
 
 	if (isLogin) {
 		return await resolve(event);
