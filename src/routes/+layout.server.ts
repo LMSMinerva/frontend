0995984c40a6ba16c7accf546e6934546f503e22
imlargo <jlargob@unal.edu.co>
2025-02-18 08:44:50 -0500

    feat: mock user data in layout server for testing purposes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,17 @@
 import type { LayoutServerLoad } from './$types';
 import type { User } from '$types/user';
-import { AuthCookies } from '$lib/server/auth';
 
 export const load = (async ({ cookies }) => {
 	const authToken = cookies.get('authToken');
 	const refreshToken = cookies.get('refreshToken');
 
-	const user: User | null = AuthCookies.getUserFromCookies(cookies);
+	const user: User = {
+		username: 'jlargo',
+		fullname: 'Juan Carlos Largo Buenahora',
+		email: 'jlargo@example.com',
+		avatar:
+			'https://lh3.googleusercontent.com/a/ACg8ocIX32rPXI4A4mrR7Jh7EMnpvVSYydEn8KqX7mIrdO0l6MvFYgBE=s96-c'
+	};
 
 	return {
 		user,
