443bcd56745b045b7ca662dc6c5e68767fc4ad3d
imlargo <jlargob@unal.edu.co>
2025-02-18 09:53:56 -0500

    feat: add optional chaining for user fullname display in the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 </svelte:head>
 
 <main class="flex flex-col gap-8 px-12">
-	<h2 class="text-2xl font-semibold">Bienvenid@, {user.fullname}</h2>
+	<h2 class="text-2xl font-semibold">Bienvenid@, {user?.fullname || "Estudiante"}</h2>
 
 	<div class="grid grid-cols-12 gap-x-12">
 		<div class="col-span-8 flex flex-col gap-2">
